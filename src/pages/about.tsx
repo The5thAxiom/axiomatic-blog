8307741fa32a990ba2109cd96225f21fec04b75c
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-04-22 16:55:12 +0530

    added about page content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,18 @@ export default function About() {
     return (
         <>
             <h1>About</h1>
+            <p>
+                Made with ❤️ by <a href='https://www.samridh.live'>Samridh</a>
+            </p>
+            <h2>Copyright</h2>
+            <p>
+                The source code and design of the website is open source under
+                GNU GPL v2. Feel free to use them if your project is also under
+                the same license.
+                <br />
+                The content of the individual blog posts is Copyrighted. Do not
+                use them without my permission.
+            </p>
         </>
     );
 }
